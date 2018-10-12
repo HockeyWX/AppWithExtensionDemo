31fc6484111927c9b93a2d69b2660c48f9e056dc
Chunxiao <luckychunxiao@gmail.com>
2018-10-12 13:48:57 +0800

    change script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp ./ProvisioningProfile/79ff9237-48b5-4643-942c-7a32594747c6.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+cp ./ProvisioningProfile/PP_AppExtensionTodayExtension.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
 echo "Copied Extension Provisioning Profile.";
 for i in $(ls ~/Library/MobileDevice/Provisioning\ Profiles); do echo "$i"; done;
