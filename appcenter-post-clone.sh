bfd0879ed81ad3c6c1684248a8d4be970a8e3612
Chunxiao <luckychunxiao@gmail.com>
2018-10-16 14:30:46 +0800

    update

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp ./ProvisioningProfile/PP_AppExtensionTodayExtension.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+cp ./ProvisioningProfile/AppWithExtension_TodayExtension_AppStore_PP.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
 echo "Copied Extension Provisioning Profile.";
 for i in $(ls ~/Library/MobileDevice/Provisioning\ Profiles); do echo "$i"; done;
