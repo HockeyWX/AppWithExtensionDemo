ddf531e7808e8f4d85a381fcacae92d519bf6718
Kevin <chunxli@microsoft.com>
2019-02-07 16:49:20 +0800

    Add NotificationServiceExtension

---
+++ Notification/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  Notification
+//
+//  Created by Dev Wuxi on 2019/2/7.
+//  Copyright © 2019 Kevin Li. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
